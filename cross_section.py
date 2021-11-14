e30c01a4728d84a4bc8c28546eb60937d611f8fb
fabianzoepf <fzoepf@web.de>
2021-11-14 10:24:25 +0100

    adapt parameters to updated input image quality

--- cross_section.py
+++ cross_section.py
@@ -1,5 +1,4 @@
 """Extract cross sections of detected objects along their main axis through their origin."""
-import argparse
 from typing import Tuple
 
 import numpy as np
@@ -19,10 +18,10 @@ def denoise(binary_image: np.ndarray) -> np.ndarray:
         np.ndarray: denoised binary image
     """
 
-    closing_kernel = np.ones((8, 8), np.uint8)
+    closing_kernel = np.ones((30, 30), np.uint8)
     closed = cv2.morphologyEx(binary_image, cv2.MORPH_CLOSE, closing_kernel)
 
-    opening_kernel = np.ones((8, 8), np.uint8)
+    opening_kernel = np.ones((60, 60), np.uint8)
     opened = cv2.morphologyEx(closed, cv2.MORPH_OPEN, opening_kernel)
 
     return opened
@@ -95,46 +94,12 @@ def get_orientation(contour: np.ndarray) \
     # principal component analysis
     pca = PCA(n_components=1).fit(contour)
 
-    mean = pca.mean_
+    mean = pca.mean_.astype(int)
     eigenvector = pca.components_[0]
     eigenvalue = pca.explained_variance_[0]
     angle = np.arctan2(eigenvector[1], eigenvector[0])
 
     return (angle, mean, eigenvector, eigenvalue)
-
-
-def get_origin(section: np.ndarray, offset: Tuple[int, int]) -> np.ndarray:
-    """
-    Calculate the mean origin of all detected circles.
-    Find circles using a Hough transformation and
-    take the average center of all detected circles.
-
-    If the Hough transform fails, return (0, 0).
-
-    Args:
-        section (np.ndarray): input image part
-
-        offset (Tuple[int, int]): offset of the upper left image part
-            edge relative to the complete image
-
-    Returns:
-        origin (np.ndarray): origin coordinates
-    """
-    # init centroid in case calculation fails
-    origin_x = 0
-    origin_y = 0
-
-    # find circles/ellipses
-    circles = cv2.HoughCircles(section, cv2.HOUGH_GRADIENT, 1, 5, 
-                param1=128, param2=20, minRadius=20, maxRadius=30)
-
-    if circles is not None:
-        centers = circles[0, :, :2]
-        # calc centroid as mean of all detected circles
-        origin_x = int(np.mean(centers[:, 0])) + offset[0]
-        origin_y = int(np.mean(centers[:, 1])) + offset[1]
-
-    return np.array([origin_x, origin_y])
 
 
 def get_cross_section(section: np.ndarray, offset: Tuple[int, int],
@@ -165,9 +130,13 @@ def get_cross_section(section: np.ndarray, offset: Tuple[int, int],
 
     rotated = ndimage.rotate(padded_section, np.rad2deg(angle), reshape=False)
 
-    cross_section = rotated[int(rotated.shape[0] / 2), :]
+    cross_sec_left = np.trim_zeros(rotated[int(rotated.shape[0] / 2), :int(rotated.shape[1] / 2)])
+    cross_sec_right = np.trim_zeros(rotated[int(rotated.shape[0] / 2), int(rotated.shape[1] / 2):])
 
-    return np.trim_zeros(cross_section)
+    if len(cross_sec_right) >= len(cross_sec_left):
+        return cross_sec_right
+    else:
+        return np.flip(cross_sec_left)
 
 
 def plot_image(image: np.ndarray, plot_data: Tuple[np.ndarray, np.ndarray,
@@ -177,6 +146,8 @@ def plot_image(image: np.ndarray, plot_data: Tuple[np.ndarray, np.ndarray,
     after plotting until key is pressed
 
     Args:
+        image (np.ndarray): input image
+
         plot data (Tuple[np.ndarray, np.ndarray, Tuple[int, int, int, int], np.ndarray]:
             tuple containing
 
@@ -188,38 +159,29 @@ def plot_image(image: np.ndarray, plot_data: Tuple[np.ndarray, np.ndarray,
     # convert image to color
     rgb = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
 
-    for (origin, eigenvec, box, cntr) in plot_data:
+    for i, (origin, eigenvec, eigenval, box, cntr) in enumerate(plot_data):
         x, y, w, h = box
         # plot bounding box
-        cv2.rectangle(rgb, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        cv2.rectangle(rgb, (x, y), (x+w, y+h), (0, 0, 255), 4)
+        cv2.putText(rgb, str(i), (x, y+h), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 8)
 
         # plot contour
-        cv2.drawContours(rgb, cntr, -1, (0, 255, 0), 4)
+        cv2.drawContours(rgb, cntr, -1, (0, 255, 0), 8)
 
-        # plot extracted lines
-        len_x = (origin[0] - x)
-        len_y = (origin[1] - y)
+        cv2.arrowedLine(rgb, 
+            (origin[0], origin[1]),
+            (int(origin[0] + eigenvec[0] * np.sqrt(eigenval)),
+             int(origin[1] + eigenvec[1] * np.sqrt(eigenval))),
+            (255, 0, 0), 8)
 
-        if len_x > len_y:
-            m = eigenvec[1] / eigenvec[0]
-            cv2.line(rgb, 
-                (x, int((x - origin[0]) * m + origin[1])),
-                ((x + w), int(((x + w) - origin[0]) * m + origin[1])),
-                (255, 0, 0), 4)
-        else:
-            m = eigenvec[0] / eigenvec[1]
-            cv2.line(rgb, 
-                (int((y - origin[1]) * m + origin[0]), y),
-                (int(((y + h) - origin[1]) * m + origin[0]), (y + h),),
-                (255, 0, 0), 4)
-
+    rgb = cv2.resize(rgb, (0,0), fx=0.2, fy=0.2)
     cv2.imshow('image', rgb)
     cv2.waitKey(0)
 
 
 def main(image: np.ndarray, min_box_size: int, plot: bool=False) -> list:
     """
-    Extract cross sections of each object in the image, where the origin of the 
+    Extract cross sections of each object in the image, where the origin of the
     cross section is the average center of all circles/ellipses in the object
     and the cross section is extracted along the main principal component.
 
@@ -236,25 +198,25 @@ def main(image: np.ndarray, min_box_size: int, plot: bool=False) -> list:
     mask = threshold_image(image)
     contours, bounding_boxes = get_sections(mask, min_box_size)
 
+    # invert image
+    inverted = cv2.bitwise_not(image)
+    # black out background
+    inverted[mask == 0] = 0
+
     cross_sections = []
     plot_data = []
     for (cntr, box) in zip(contours, bounding_boxes):
         x, y, w, h = box
-        section = image[y:y+h, x:x+w]
+        section = inverted[y:y+h, x:x+w]
 
-        origin = get_origin(section, (x, y))
-        angle, mean, eigenvec, _ = get_orientation(cntr)
-
-        # fallback for origin, in case calculation failed: take mean from PCA
-        if origin[0] == 0 and origin[1] == 0:
-            origin = mean
+        angle, origin, eigenvec, eigenval = get_orientation(cntr)
 
         line = get_cross_section(section, (x, y), origin, angle)
 
         cross_sections.append(line)
 
         if plot:
-            plot_data.append((origin, eigenvec, box, cntr))
+            plot_data.append((origin, eigenvec, eigenval, box, cntr))
 
     if plot:
         plot_image(image, plot_data)
@@ -263,12 +225,14 @@ def main(image: np.ndarray, min_box_size: int, plot: bool=False) -> list:
 
 
 if __name__ == '__main__':
+    import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument('image_path', help='image to be analyzed')
     parser.add_argument('--min_box_size', help='minimal bounding box height/width',
-        default=50, required=False)
+        default=200, required=False)
     args = parser.parse_args()
 
     image = cv2.imread(args.image_path, cv2.IMREAD_GRAYSCALE)
 
-    main(image, args.min_box_size, True)
+    lines = main(image, args.min_box_size, True)
